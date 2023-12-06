df343ff43650ded2a982d3b477a8f186161f73a5
Robert Hopkins <rmhopkins4@gmail.com>
2023-12-06 16:04:06 -0500

    Update island_cleft_demo.py

--- island_cleft_demo.py
+++ island_cleft_demo.py
@@ -6,7 +6,7 @@ import numpy as np
 from nltk.parse import generate
 import matplotlib.pyplot as plt
 
-
+"""
 # load
 island_sentence_tuples = gc.corpus_from_json(
     "grammar_outputs/island_cleft/tuples.json", True)
@@ -18,7 +18,7 @@ island_sentence_tuples = gc.corpus_from_json(
 # save
 gc.corpus_to_json(island_sentence_tuples,
                   "grammar_outputs/island_cleft/tuples_gpt2.json")
-
+"""
 
 # load
 tuple_data = gc.corpus_from_json(
@@ -26,29 +26,31 @@ tuple_data = gc.corpus_from_json(
 
 # extract
 island_gap_effects = []
-islandless_gap_effects = []
+extractible_gap_effects = []
 for sentence_tuple in tuple_data:
     island_gap_effects.append(  # (island, no gap) - (island, gap)
         sentence_tuple.s_xb.critical_surprisal - sentence_tuple.s_xx.critical_surprisal
     )
-    islandless_gap_effects.append(  # (no island, no gap) - (no island, gap)
+    extractible_gap_effects.append(  # (no island, no gap) - (no island, gap)
         sentence_tuple.s_ab.critical_surprisal - sentence_tuple.s_ax.critical_surprisal)
 
 # average
 avg_island_gap_effect = np.mean(island_gap_effects)
-avg_islandless_gap_effect = np.mean(islandless_gap_effects)
+avg_extractible_gap_effect = np.mean(extractible_gap_effects)
 
 # plot
 fig, ax = plt.subplots()
 ax.bar(
     [
-        "Effect of Extractible Gap (-Island)",
-        "Effect of Stranded Gap (+Island)"
+        "Effect of Control Gap (-Island)",
+        "Effect of Island Gap"
     ],
     [
-        avg_islandless_gap_effect,
+        avg_extractible_gap_effect,
         avg_island_gap_effect
     ]
 )
-
+plt.suptitle("Effects of Gaps on Critical Surprisal in Clefted Sentences")
+plt.title("Less effect expected in +Island constructions\n as clefting from within an island is not possible.",
+          fontdict={'size': 8})
 plt.show()
