7402cc1d70488e90ab1eb10faaaf20dac9e88108
Sathvik Nair <sathvik@umd.edu>
2024-03-11 17:18:51 -0400

    updating paths

--- scripts/retrain_grnn.sh
+++ scripts/retrain_grnn.sh
@@ -9,4 +9,4 @@ nvidia-smi
 python3 cuda_check.py
 MODEL_NAME=
 LOG_NAME=
-python3 colorlessgreenRNNs/src/language_models/main.py --data /fs/clip-psych/sathvik/lm-syntactic-generalization/grnn_data --model LSTM --emsize 650 --nhid 650 --nlayers 2 --dropout 0.2 --lr 20 --batch_size 128 --cuda --save $MODEL_NAME --log $LOG_NAME
+python3 /fs/clip-psych/sathvik/lm-syntactic-generalization/colorlessgreenRNNs/src/language_models/main.py --data /fs/clip-psych/sathvik/lm-syntactic-generalization/grnn_data --model LSTM --emsize 650 --nhid 650 --nlayers 2 --dropout 0.2 --lr 20 --batch_size 128 --cuda --save $MODEL_NAME --log $LOG_NAME
